d6d7a62583869c779b116aa92e9d23af369fc59c
Lou Markovski <lou.markovski@gmail.com>
2014-10-07 16:45:11 +1100

    dev
    * Add results page for lat/long points search

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-META_REST_URL = 'http://172.20.10.14:5150/geolib/meta/v0.1/'
-FREE_TEXT_URL = 'http://172.20.10.14:5150/geolib/meta/v0.1/search?'
-POINTS_API = 'http://172.20.10.14:5150/geolib/image/v0.1/points?'
+REST_IP = '<your_rest_ip>'
+REST_PORT = '<your_rest_port>'
+REST_SERVER = '%s:%s' % (REST_IP, REST_PORT)
+
+META_REST_URL = 'http://%s/geolib/meta/v0.1/' % REST_SERVER
+FREE_TEXT_URL = 'http://%s/geolib/meta/v0.1/search?' % REST_SERVER
+POINTS_API = 'http://%s/geolib/image/v0.1/points?' % REST_SERVER
